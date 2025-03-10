20cd30424f78304619c1ab74b3b25ba454b62113
Max042004 <sau525@gmail.com>
2025-03-10 17:11:04 +0800

    fix: Separate known failing test into an xfail test

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,26 +1,28 @@
-import ...
-from ...
+import pytest
+from solution import Solution  # or however you're importing
 
-@pytest.fixture
-...
-    
 testcases = [
     # s, p, expected_res
-    ["", "", True],
-    ["aa", "a", False],
-    ["aa", "a*", True],
-    ["ab", ".*", True],
-    ["a", ".*.", True],
-    ["aab", "c*a*b", True],
-    ["aaa", "ab*a*c*a", True]
+    ("", "", True),
+    ("aa", "a", False),
+    ("aa", "a*", True),
+    ("ab", ".*", True),
+    ("aab", "c*a*b", True),
+    ("aaa", "ab*a*c*a", True)
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
+@pytest.fixture
+def solution():
+    return Solution()
+
+@pytest.mark.parametrize("s,p,expected", testcases)
+def test_regex(solution, s, p, expected):
+    """Test the 6 cases that are passing."""
     assert solution.isMatch(s, p) == expected
 
-"""
 @pytest.mark.xfail
-def test_broken_solution(solution):
-    assert solution.isMatch() == 
-"""
+def test_broken_case(solution):
+    """Test the single case that is known to fail."""
+    s, p, expected = ("a", ".*.", True)
+    assert solution.isMatch(s, p) == expected
+
